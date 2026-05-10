31bf9a7af74d25a66f2d7e735fccbfd7c6187e21
Ruy@DustyCanteen <dustycanteen@gmail.com>
2026-05-10 22:00:02 +0800

    Specify correct weights

--- infer.bash
+++ infer.bash
@@ -1,7 +1,7 @@
 python -u ./entry.py \
   --mode=infer \
   --data_path=../hw3-data-release \
-  --checkpoint=../checkpoints/e_9_ap50_0.67.pt \
+  --checkpoint=../checkpoints/e_14_ap50_0.73.pt \
   --crop_size=256 \
   --score_threshold=0.05 \
   --mask_threshold=0.5 \
