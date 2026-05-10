d170177a00d3b8f95c8049ea56e5e002988622fc
Ruy@DustyCanteen <dustycanteen@gmail.com>
2026-05-10 21:58:15 +0800

    Update inference crop to match training crop

--- infer.bash
+++ infer.bash
@@ -2,7 +2,7 @@ python -u ./entry.py \
   --mode=infer \
   --data_path=../hw3-data-release \
   --checkpoint=../checkpoints/e_9_ap50_0.67.pt \
-  --crop_size=512 \
+  --crop_size=256 \
   --score_threshold=0.05 \
   --mask_threshold=0.5 \
   --output_path=test-results.json
